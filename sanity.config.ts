3a56b12e2c0d2a8ff9561a8367ede59de44cc528
Tim <tim@haywood.org>
2023-04-08 20:55:57 +1000

    fix: type errors

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { schemaTypes } from "./schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 export default defineConfig({
